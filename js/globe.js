aa145f8062df1f45a59b2c24cfa5f1294cad44c1
Michael Fine <mfine15@gmail.com>
2017-11-07 20:24:42 -0500

    prune, add circle sizes

--- js/globe.js
+++ js/globe.js
@@ -1,8 +1,7 @@
 var isSmall = window.innerWidth < 720; 
-var width  =  isSmall? 360 : 540;
-var height = isSmall? 360 : 540;
+var width  =  Math.min(window.innerWidth, 540);
+var height =  isSmall? 270 : 540;
 
-var rScale = d3.scaleSqrt();
 var peoplePerPixel = 50000;
 var max_population = [];
 var links = [];
@@ -18,114 +17,18 @@ var sens = 0.25;
 var dragging = false;
 // set projection type and paremeters
 var projection = d3.geoOrthographic()
-   .scale(isSmall? 160 : 220)
+   .scale(isSmall? 110 : 220)
    .translate([(width / 2), height / 2])
    .clipAngle(90);
-var path = d3.geoPath().projection(projection).pointRadius(1);
+var path = d3.geoPath().projection(projection);
 
-// var sky = d3.geoOrthographic()
-//    .translate([width / 2, height / 2])
-//    .clipAngle(90)
-//    .scale(260);
-
-// var swoosh = d3.line()
-//      .x(function(d) { return d[0] })
-//      .y(function(d) { return d[1] })
-//      .curve(d3.curveCatmullRom);
-function swoosh(d){
-  // console.log(d);
-  return { 
-    "type": "Feature", 
-    "geometry": {
-      "type": "LineString",
-      "coordinates": d
-    }
-  };
-}
-var arc = d3.line()
-     .x(function(d) { return d[0] })
-     .y(function(d) { return d[1] })
-    //  .tension(.0);
-function flying_arc(pts) {
-  var source = pts.coords[0],
-      target = pts.coords[1];
-  // get canvas coords of arc midpoint and globe center
-  var mid = projection(location_along_arc(source, target, .5));
-  var ctr = projection.translate();
-
-  // max length of a great circle arc is π,
-  // so 0.3 means longest path "flies" 20% of radius above the globe
-  var scale = 1 + 0.3 * d3.geoDistance(source,target) / Math.PI;
-  mid[0] = ctr[0] + (mid[0]-ctr[0])*scale;
-  mid[1] = ctr[1] + (mid[1]-ctr[1])*scale;
-
-  var result = [ projection(source),
-                 // mid,
-                 projection(target) ]
-  return result;
-}
 
 function location_along_arc(start, end, loc) {
   var interpolator = d3.geoInterpolate(start,end);
   return interpolator(loc)
 }
 
-// TODO: Function to clip by bounding box,
-// currently clip finds the cartesian point of intersection with the 90 meredian
-function clipArc(p1, p2){
-  const [x1,y1] = projection(p1);
-  const [x2,y2] = projection(p2);
-  const meridian = projection([45, 90])[0];
 
-  const slope = (y2 - y1) / (x2 - x1);
-  const clip = [meridian, (meridian - x1) * slope + y1];
-  const relative = location_along_arc(p1, p2, loc);
-
-}
-
-
-function fade_at_edge(d) {
-  var centerPos = projection.invert([width/2,height/2]),
-      start, end;
-  // function is called on 2 different data structures..
-  if (d.coords && d.coords[0]) {
-    start = d.coords[0],
-    end = d.coords[1];
-  }
-  else {
-    start = d.geometry.coordinates[0];
-    end = d.geometry.coordinates[1];
-  }
-  var start_dist = Math.PI * 2/3 - d3.geoDistance(start,centerPos),
-      end_dist = Math.PI * 2/3 - d3.geoDistance(end,centerPos);
-
-  var fade = d3.scaleLinear().domain([-.1,0]).range([0,.1])
-  var dist = start_dist < end_dist ? start_dist : end_dist;
-
-  return fade(dist)
-}
-
-function hide_off(d) {
-  var centerPos = projection.invert([width/2,height/2]),
-      start, end;
-  // function is called on 2 different data structures..
-  if (d.coords[0]) {
-    start = d.coords[0],
-    end = d.coords[1];
-  }
-  else {
-    console.log(d);
-    start = d.geometry.coordinates[0];
-    end = d.geometry.coordinates[1];
-  }
-  console.log()
-  var start_dist = 1.57 - d3.geoDistance(start,centerPos),
-      end_dist = 1.57 - d3.geoDistance(end,centerPos);
-
-  return start_dist > 0 || end_dist > 0? 'inline' : 'none';
-}
-
-// create path variable, empty svg element and group container
    var svg = d3.select("#globe").append("svg")
               .attr("width", width)
               .attr("height", height)
@@ -143,9 +46,13 @@ var g = svg.append("g");
 
 function makePairs(data,times){
   let pairs = [];
+  let points = [];
   let i = 0;
   while(pairs.length < times){
     let center = _.sample(data.features);
+
+    points.push(Object.assign(center, center.properties));
+
     _.times(Math.pow(i,2), () => {
       let branch = _.sample(data.features);
       pairs.push({
@@ -154,18 +61,20 @@ function makePairs(data,times){
           branch.geometry.coordinates
         ],
         pop: [
-          branch.properties.population,
           center.properties.population,
+          branch.properties.population,
         ],
         points: [
           center.geometry,
           branch.geometry
         ]
       });
+      points.push(Object.assign(branch, branch.properties));
+
     })
     i++;
   };
-  return pairs;
+  return [pairs,points];
 }
 
 
@@ -194,10 +103,8 @@ queue()
       .attr("class", "graticule")
       .attr("d", path);
 
-   // setting the circle size (not radius!) according to the number of inhabitants per city
 
-
-   links = makePairs(data,30);
+   let [links, points] = makePairs(data,30);
 
    links.forEach(function(e,i,a) {
      var feature = { 
@@ -210,111 +117,59 @@ queue()
     }
      arcLines.push(feature)
    });
-   console.log(_.flatten(_.map(links, 'points')));
+ 
+  const rScale = d3.scaleLog()
+    .domain(d3.extent(points, p => p.population))
+    .range([2, 7]);
 
-   // Drawing transparent circle markers for cities
-
-
-   // start spinning!
-  // svg.append("g").attr("class","arcs")
-  //   .selectAll("path").data(arcLines)
-  //   .enter().append("path")
-  //    .attr("class","arc")
-  //    .attr("d",path);
-  const weight = d3.scaleQuantize().domain([0,1]).range([1,2,3]);
-
-  var defs = svg.append("defs");
-
-  //Code taken from http://stackoverflow.com/questions/9630008/how-can-i-create-a-glow-around-a-rectangle-with-svg
-  //Filter for the outside glow
-  var filter = defs.append("filter")
-    .attr("id","glow");
-
-  filter.append("feGaussianBlur")
-    .attr("class", "blur")
-    .attr("stdDeviation",glow)
-    .attr("result","coloredBlur");
-
-  var feMerge = filter.append("feMerge");
-  feMerge.append("feMergeNode")
-    .attr("in","coloredBlur");
-  feMerge.append("feMergeNode")
-    .attr("in","SourceGraphic");
-
+  path.pointRadius(function(d) {
+    console.log("pointRadius",d, d.population, rScale(d.population));
+    return d.population ? rScale(d.population) : 1;
+  });
+  
+  console.log("points",points);
   svg.append("g")
    .attr("class","points")
    .selectAll("text")
-   .data(_.flatten(_.map(links, 'points')))
+   .data(points)
    .enter().append("path")
    .attr("class", "point")
-   // .style("filter","url(#glow)")
-   .attr("d", path);
+   .attr("d", path)
+   .attr("fill", "#6400e3")
+   .attr("fill-opacity", 0.4);
 
   svg.append("g").attr("class","flyers")
     .selectAll("path").data(arcLines)
     .enter().append("path")
     .attr("id" , (d, i) => "flyer" + i)
     .attr("class","flyer")
-    // .style("filter","url(#glow)")
-    // .attr("d", function(d) { return swoosh(flying_arc(d)) })
     .attr("d", path)
     .attr('stroke-width', d => {
-      console.log("setting weight", d.weight, weight(d.weight));
-      return weight(d.weight)
+      return d.weight * 10
     })
-    // .attr("opacity", function(d) {
-    //     return fade_at_edge(d)
-    // })
 
-  // svg.append("g").attr("class","arcs")
-    // .selectAll("path").data(arcLines)
-    // .attr("id" , (d, i) => "arc" + i)
-    // .enter().append("path")
-    // .attr("class","arc")
-    // .attr("d", path)
-    // .attr("opacity", function(d) {
-    //     return fade_at_edge(d)
-    // });
 
   d3.selectAll(".flyer").each(function(d,i){
     var totalLength = d3.select("#flyer" + i).node().getTotalLength();
       d3.selectAll("#flyer" + i).attr("stroke-dasharray", totalLength + " " + totalLength)
         .attr("stroke-dashoffset", totalLength)
         .transition()
+        .on("end", () => {
+          d3.selectAll("#flyer" + i)
+            .attr("stroke-dasharray", 0)
+        })
         .duration(4000)
         .delay(80*i)
         .attr("stroke-dashoffset", 0)
-        // .style("stroke-width",1)
+        .attr('stroke-width', d => {
+          return d.weight * 10
+        })
   })
-  d3.selectAll(".arc").each(function(d,i){
-    var totalLength = d3.select("#arc" + i).node().getTotalLength();
-      d3.selectAll("#arc" + i).attr("stroke-dasharray", totalLength + " " + totalLength)
-        .attr("stroke-dashoffset", totalLength)
-        .transition()
-        .duration(4000)
-        .delay(80*i)
-        .attr("stroke-dashoffset", 0)
-        // .style("stroke-width",1)
-  })
+
 
    refresh();
 });
 
-// function refresh() {
-//   svg.selectAll(".land").attr("d", path);
-//   svg.selectAll(".point").attr("d", path);
-//
-//   svg.selectAll(".arc").attr("d", path)
-//     .attr("opacity", function(d) {
-//         return fade_at_edge(d)
-//     })
-//
-//   svg.selectAll(".flyer")
-//     .attr("d", function(d) { return swoosh(flying_arc(d)) })
-//     .attr("opacity", function(d) {
-//       return fade_at_edge(d)
-//     })
-// }
 
 function refresh(){
   d3.timer(function(elapsed) {
@@ -326,7 +181,9 @@ function refresh(){
       }
       // sky.rotate([rotate[0] + velocity[0] * elapsed, rotate[1]+ velocity[1] * elapsed]);
       svg.selectAll(".land").attr("d", path);
-      svg.selectAll(".point").attr("d", path);
+      svg.selectAll(".point")
+        .attr("d", path)
+
       svg.selectAll("path").attr("d",path);
       svg.selectAll(".arc")
         .attr("d", path)
@@ -339,54 +196,8 @@ function refresh(){
         .attr("opacity", function(d) {
           return fade_at_edge(d)
         })
-      svg.selectAll("path.cities").attr("d", path);
-      svg.selectAll(".point").attr("d", path);
+      }
 
-      d3.selectAll(".flyer").each(function(d,i){
-        var totalLength = d3.select("#flyer" + i).node().getTotalLength();
-          d3.selectAll("#flyer" + i).attr("stroke-dasharray", totalLength + " " + totalLength)
-            .attr("stroke-dashoffset", totalLength)
-            .attr("stroke-dashoffset", 0)
-            // .style("stroke-width",1)
-      })
-      d3.selectAll(".arc").each(function(d,i){
-        var totalLength = d3.select("#arc" + i).node().getTotalLength();
-          d3.selectAll("#arc" + i).attr("stroke-dasharray", totalLength + " " + totalLength)
-            .attr("stroke-dashoffset", totalLength)
-            .attr("stroke-dashoffset", 0)
-            // .style("stroke-width",1)
-      })
-    }
-
-},50);
+},100);
 
 }
-
-// Events for sliders and button
-// document.getElementById("rotation").addEventListener("change", function() {
-//    var new_speed = this.value;
-//    velocity[0] = new_speed
-// });
-//
-// document.getElementById("glow").addEventListener("change", function() {
-//    var new_glow = this.value;
-//    g.selectAll("path.cities")
-//    .attr("fill-opacity", new_glow);
-// });
-//
-// document.getElementById("marker_size").addEventListener("change", function() {
-//    var new_marker_size = 1 / this.value ;
-//    peoplePerPixel = new_marker_size * 100000;
-//    var rMin = 0;
-//    var rMax = Math.sqrt(max_population / (peoplePerPixel * Math.PI));
-//    rScale.range([rMin, rMax]);
-// });
-//
-// document.getElementById("color").addEventListener("change", function() {
-//    var new_color = this.value;
-//    g.selectAll("path.cities")
-//    .attr("fill", new_color);
-// });
-
-// hackish approach to get bl.ocks.org to display individual height
-d3.select(self.frameElement).style("height", height + "px");
